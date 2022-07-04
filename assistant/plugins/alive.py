3a190b93b549721492122a876833dfc9035eafaa
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-07-05 00:52:24 +0530

    Need update

--- assistant/plugins/alive.py
+++ assistant/plugins/alive.py
@@ -17,31 +17,12 @@ from assistant import bot, cus_filters, versions
 from assistant.bot import START_TIME
 from assistant.utils import time_formatter
 
-LOGO_DATA = []
-MSG_IDS = [499509, 499428, 496502, 496360, 496498]
 
+file_id = [
+]
 
 @bot.on_message(filters.command("alive") & cus_filters.auth_chats)
-async def _alive(_, message: Message):
-    try:
-        await _sendit(message.chat.id)
-    except (FileIdInvalid, FileReferenceEmpty, BadRequest):
-        await _refresh_data()
-        await _sendit(message.chat.id)
-
-
-async def _refresh_data():
-    LOGO_DATA.clear()
-    for msg in await bot.get_messages('UserGeOt', MSG_IDS):
-        if not msg.animation:
-            continue
-        gif = msg.animation
-        LOGO_DATA.append(gif.file_id)
-
-
-async def _sendit(chat_id):
-    if not LOGO_DATA:
-        await _refresh_data()
+async def _alive(_, message: Message, chat_id)
     caption = f"""
 **🤖 Bot Uptime** : `{time_formatter(time.time() - START_TIME)}`
 **🤖 Bot Version** : `{versions.__assistant_version__}`
@@ -53,15 +34,15 @@ async def _sendit(chat_id):
                 InlineKeyboardButton(
                     text="License",
                     url=("https://github.com/"
-                         "UsergeTeam/Userge-Assistant/blob/master/LICENSE")),
+                         "Team/-Deadly/DEADLY-SPAMBOT/blob/master/LICENSE")),
                 InlineKeyboardButton(
                     text="Repo",
-                    url="https://github.com/UsergeTeam/Userge-Assistant")
+                    url="https://github.com/Team-Deadly/DEADLY-SPAMBOT")
             ]
         ]
     )
     file_id = random.choice(LOGO_DATA)
-    await bot.send_animation(chat_id=chat_id,
-                             animation=file_id,
+    await bot.reply_photo(chat_id=chat_id,
+                             photo=file_id,
                              caption=caption,
                              reply_markup=button)
